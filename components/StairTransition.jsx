c81163b4260c6058b3afff851de0648d9cd70c03
IT21818202 <it21818202@my.sliit.lk>
2024-06-21 01:25:57 +0530

    added more features to the navbar

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,23 +1,35 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion"
-import { usePathname } from "next/navigation"
+import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+// components
+import Stairs from "./Stairs";
 
 const StairTransition = () => {
-    const pathname = usePathname();
-    return (
-        <>
-            <AnimatePresence mode="wait">
-                <div key={pathname}>
-                    <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
-                        <Stairs>
-                    </div>
-                </div>
-            </AnimatePresence>
-        </>
-    )
-}
+  const pathname = usePathname();
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        <div key={pathname}>
+          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+            <Stairs />
+          </div>
 
-export default StairTransition
+          <motion.div
+            className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+              transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+            }}
+          />
+        </div>
+      </AnimatePresence>
+    </>
+  );
+};
 
-{/* stopped at 32:3 */}
+export default StairTransition;
+
+// stopped at 38:24
