b5d10827b1831872fb6f2dadd0bdc9e93e8e0500
Jehan16 <jehanshenil16@gmail.com>
2024-06-24 23:25:57 +0530

    added the packages

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 // components
