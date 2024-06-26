b2324d657d11bd6462bd2fb55ed031257b6c5f88
Jehan16 <jehanshenil16@gmail.com>
2024-06-26 23:29:40 +0530

    added the navbar collaps feature

---
+++ components/MobileNav.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link';
+import { CiMenuFries } from 'react-icons/ci';
+
+const Links = [
+    {
+        name: 'home',
+        path: '/'
+    },
+    {
+        name: 'services',
+        path: '/services'
+    },
+    {
+        name: 'resume',
+        path: '/resume'
+    },
+    {
+        name: 'work',
+        path: '/work'
+    },
+    {
+        name: 'contact',
+        path: '/contact'
+    }
+]
+
+const MobileNav = () => {
+    const pathname = usePathname()
+
+    return (
+        <Sheet>
+            <SheetTrigger className='flex justify-center items-center'>
+                <CiMenuFries className='text-[32px] text-accent' />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+                {/* logo */}
+                <div>Logo</div>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileNav
