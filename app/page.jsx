e8f8b49a0bad3f694c968b6c98dd946c45102565
Jehan16 <jehanshenil16@gmail.com>
2024-06-27 23:54:18 +0530

    added the socials icons to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi"
 
+// components
+import Socials from "@/components/Socials"
+
 const Home = () => {
   return (
     <section className="h-full">
@@ -16,11 +19,19 @@ const Home = () => {
               I excel at crafting elegant digital experiences and I am proficient in varies programming languages and technologies.
             </p>
             {/* button and socials */}
-            <div>
-              <Button>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2">
                 <span>Download CV</span>
-                <FiDownload />
+                <FiDownload className="text-xl" />
               </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+              </div>
             </div>
           </div>
           {/* photo */}
@@ -32,5 +43,3 @@ const Home = () => {
 }
 
 export default Home
-
-// stopped at 56:59
