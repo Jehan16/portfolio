8eb372ca305f1d76ca13bf273ed843939b8d4765
Jehan16 <jehanshenil16@gmail.com>
2024-06-27 17:26:24 +0530

    added the title and the about section to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,36 @@
-export default function Home() {
+import { Button } from "@/components/ui/button"
+import { FiDownload } from "react-icons/fi"
+
+const Home = () => {
   return (
-    <main>homepage</main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I`m <br /> <span className="text-accent">Jehan Silva</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am proficient in varies programming languages and technologies.
+            </p>
+            {/* button and socials */}
+            <div>
+              <Button>
+                <span>Download CV</span>
+                <FiDownload />
+              </Button>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
+
+// stopped at 56:59
