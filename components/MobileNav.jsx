414772d659b3a8a019726138f68029f4b012c61b
Jehan16 <jehanshenil16@gmail.com>
2024-06-27 16:45:09 +0530

    added the motion transitions

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -38,7 +38,21 @@ const MobileNav = () => {
             </SheetTrigger>
             <SheetContent className="flex flex-col">
                 {/* logo */}
-                <div>Logo</div>
+                <div className='mt-32 mb-40 text-center text-2xl'>
+                    <Link href="/">
+                        <h1 className='text-4xl font-semibold'>
+                            Jehan<span className='text-accent'>.</span>
+                        </h1>
+                    </Link>
+                </div>
+                {/* nav */}
+                <nav className='flex flex-col justify-center items-center gap-8'>
+                    {Links.map((link, index) => {
+                        return <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
+                            {link.name}
+                        </Link>
+                    })}
+                </nav>
             </SheetContent>
         </Sheet>
     )
