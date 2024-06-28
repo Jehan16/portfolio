b63dc7b96364e95c1a58696d24fa3d3536f1ae7e
Jehan16 <jehanshenil16@gmail.com>
2024-06-28 12:53:59 +0530

    completed the home page adding the about photo and stats

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import { FiDownload } from "react-icons/fi"
 
 // components
 import Socials from "@/components/Socials"
+import Photo from "@/components/ui/Photo"
+import Stats from "@/components/Stats"
 
 const Home = () => {
   return (
@@ -10,7 +12,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I`m <br /> <span className="text-accent">Jehan Silva</span>
@@ -30,18 +32,21 @@ const Home = () => {
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex 
+                  justify-center items-center text-accent text-base hover:bg-accent 
+                  hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   )
 }
 
 export default Home
-
-// stopped at 1:02:08
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-accent text-primary hover:bg-accent-hover",
         primary: "bg-primary text-white",
-        outline: "boarder boarder-accent bg-transparent text-accent hover:bg-accent hover:text-primary"
+        outline: "border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary"
       },
       size: {
         default: "h-[44px] px-6",
