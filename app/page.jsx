d47681e19f4676e84feb6b47fa3de89b1ac9e22c
Jehan16 <jehanshenil16@gmail.com>
2024-06-30 18:04:12 +0530

    this branch is created to resurve the completed unedited code

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button"
-import { FiDownload } from "react-icons/fi"
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
 // components
-import Socials from "@/components/Socials"
-import Photo from "@/components/ui/Photo"
-import Stats from "@/components/Stats"
+import Socials from "@/components/Socials";
+import Photo from "@/components/ui/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -13,19 +13,24 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-xl">Software Engineering Undergraduate</span>
             <h1 className="h1 mb-6">
               Hello I`m <br /> <span className="text-accent">Jehan Silva</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experiences and I am proficient in varies programming languages and technologies.
+              As a software engineering undergraduate, I have a passion for
+              technology and a strong desire to learn. I am a quick learner, and
+              I am always eager to expand my knowledge and skills in software
+              development. I excel at crafting elegant digital experiences and I
+              am proficient in varies programming languages and technologies.
             </p>
             {/* button and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                 variant="outline"
                 size="lg"
-                className="uppercase flex items-center gap-2">
+                className="uppercase flex items-center gap-2"
+              >
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
               </Button>
@@ -34,7 +39,8 @@ const Home = () => {
                   containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex 
                   justify-center items-center text-accent text-base hover:bg-accent 
-                  hover:text-primary hover:transition-all duration-500" />
+                  hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
@@ -44,9 +50,10 @@ const Home = () => {
           </div>
         </div>
       </div>
+      {/* stats */}
       <Stats />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
